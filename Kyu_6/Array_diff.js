d74d106f4a6d533d962adf15b301a7b460d1e276
ub-victor <victoireushindi371@gmail.com>
2026-08-22 23:49:03 +0200

    Initial Commit

--- Kyu_6/Array_diff.js
+++ Kyu_6/Array_diff.js
@@ -1,4 +1,5 @@
 /*
+Array_diff
 Implement a function that computes the difference between two lists. The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). The order of elements in the first list should be preserved in the result.
 
 Examples
