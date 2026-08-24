9c617287874e10571d6bd10f6b88f0a5189a9c22
ub-victor <victoireushindi371@gmail.com>
2026-08-24 11:20:11 +0200

    Initial Commit

--- Kyu_6/Array_diff.js
+++ Kyu_6/Array_diff.js
@@ -31,7 +31,7 @@ console.log(arrayDiff([1, 2, 2, 2, 3], [2]));   // [1, 3]
 const arrayDiff = (a,b) => {
   let res = [];
   for (const i of a){
-    if(!b.include(i)){
+    if(!b.includes(i)){
       res.push(i);
     }
   }
