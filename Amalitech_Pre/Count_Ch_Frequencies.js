f69b9948078a36d3e2d992b1f77ae6a9bc59b87f
ub-victor <victoireushindi371@gmail.com>
2026-08-26 09:25:50 +0200

    Initial Commit

--- Amalitech_Pre/Count_Ch_Frequencies.js
+++ Amalitech_Pre/Count_Ch_Frequencies.js
@@ -1,5 +1,8 @@
 /*
 Count Character Frequencies
 
+Scenario
+A system needs to analyze a username and determine how many times each character appears.
+Return an object containing the frequency of every character
 
 */
