f47c5d9a91750bf84d7bc91314d767675555bd81
ub-victor <victoireushindi371@gmail.com>
2026-08-26 09:10:00 +0200

    Initial Commit

--- Amalitech_Pre/Find_Duplicate.js
+++ Amalitech_Pre/Find_Duplicate.js
@@ -5,5 +5,5 @@ Scenario
 You are building a system that receives a list of transaction IDs.
 A transaction ID should appear only once
 
-Given an array of transaction IDs, Return the 
+Given an array of transaction IDs, Return the first ID that appears more than once
 */
