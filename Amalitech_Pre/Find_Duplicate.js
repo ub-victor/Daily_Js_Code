f3b471882e5892eef1003b9f05204413ec350ef5
ub-victor <victoireushindi371@gmail.com>
2026-08-26 09:18:47 +0200

    Initial Commit

--- Amalitech_Pre/Find_Duplicate.js
+++ Amalitech_Pre/Find_Duplicate.js
@@ -7,13 +7,13 @@ A transaction ID should appear only once
 
 Given an array of transaction IDs, Return the first ID that appears more than once
 If there is no duplicatite, return -1
-The syntax new set() create a collection of Unique values: 
-const numbers = new set([1,2,3,4,5,6,6,6]);
+The syntax new Set() create a collection of Unique values: 
+const numbers = new Set([1,2,3,4,5,6,6,6]);
 console.log(numbers)
 */
 
 const firstDuplicate = (arr) =>{
-    const seen = new set();// this will store the unique values we have seen so far
+    const seen = new Set();// this will store the unique values we have seen so far
 
     for(const num of arr){
         if(seen.has(num)){// if the number is already in the set, it means we have seen it before
