3636e11f36d3d099c8fa7866bc86a25c42010493
ub-victor <victoireushindi371@gmail.com>
2026-08-26 09:47:46 +0200

    Initial Commit

--- Amalitech_Pre/Count_Ch_Frequencies.js
+++ Amalitech_Pre/Count_Ch_Frequencies.js
@@ -14,7 +14,7 @@ const characterFrequency = (str) =>{
         if (frequency[char]){// if the character is already in the object, we increment its count
             frequency[char]++
         }else{
-            frequency[char] = 1;
+            frequency[char] = 1; // if the character is not in the object, we add it with a count of 1
         }
     }
     return frequency
